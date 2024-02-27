6eba4c312536bfe7043e1529e6ad7af2af9d9e7a
Balint Iulia Maria <balintiuliamaria@outlook.com>
2024-02-27 22:12:21 +0200

    added login function to search for user in the DB and perform the login successfully

--- pages/login/login.py
+++ pages/login/login.py
@@ -1,31 +1,48 @@
 import customtkinter
+from tkinter import messagebox
+from tkinter import *
+import bcrypt
+from db_creation import user
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
-root.geometry("500x350")
+def login_page():
+    root = customtkinter.CTk()
+    root.geometry("500x350")
 
-def login():
+    def login():
+        username = username_entry.get()
+        password = password_entry.get()
+        user_data = user.query.filter_by(username=username).first()
+        if user_data:
+            stored_password = user_data.password
+            if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
+                messagebox.showinfo("Login Successful", "Welcome back, " + username + "!")
+            else:
+                messagebox.showerror("Login Failed", "Incorrect password.")
+        else:
+            messagebox.showerror("Login Failed", "Username not found.")
+        
+
+    frame = customtkinter.CTkFrame(master = root)
+    frame.pack(pady = 20, padx = 60, expand = True)
+
+    label = customtkinter.CTkLabel(master = frame, text = "Login System")
+    label.pack(pady = 12, padx = 10)
+
+    username_entry = customtkinter.CTkEntry(master = frame, placeholder_text = "Username")
+    username_entry.pack(pady = 12, padx = 10)
+
+    password_entry = customtkinter.CTkEntry(master = frame, placeholder_text = "Password")
+    password_entry.pack(pady = 12, padx = 10)
+
+    button = customtkinter.CTkButton(master = frame, text = "Login", command = login)
+    button.pack(pady = 12, padx = 10)
+
+    checkbox = customtkinter.CTkCheckBox(master = frame, text = "Remember me")
+    checkbox.pack(pady = 12, padx = 10)
+
     print("Login Seccessfully done")
 
-
-frame = customtkinter.CTkFrame(master = root)
-frame.pack(pady = 20, padx = 60, expand = True)
-
-label = customtkinter.CTkLabel(master = frame, text = "Login System")
-label.pack(pady = 12, padx = 10)
-
-entry1 = customtkinter.CTkEntry(master = frame, placeholder_text = "Username")
-entry1.pack(pady = 12, padx = 10)
-
-entry2 = customtkinter.CTkEntry(master = frame, placeholder_text = "Password")
-entry2.pack(pady = 12, padx = 10)
-
-button = customtkinter.CTkButton(master = frame, text = "Login", command = login)
-button.pack(pady = 12, padx = 10)
-
-checkbox = customtkinter.CTkCheckBox(master = frame, text = "Remember me")
-checkbox.pack(pady = 12, padx = 10)
-
-root.mainloop()
+    root.mainloop()
