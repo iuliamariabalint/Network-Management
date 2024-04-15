8576133d2c9396523da1ab98083716e4f36e9402
Balint Iulia Maria <balintiuliamaria@outlook.com>
2024-04-15 20:11:52 +0300

    restructured the app with classes not functions

---
+++ app.py
@@ -0,0 +1,259 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+import customtkinter as ctk
+from flask import Flask
+from db_creation import db, user
+import bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/balin/Desktop/SQLite_DB/net-management.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+ctk.set_appearance_mode("dark") 
+ctk.set_default_color_theme("green")
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+       ## Setting up Initial Things
+        self.title("net-management")
+        self.geometry("720x550")
+        self.resizable(True, True)
+        #self.iconphoto(False, tk.PhotoImage(file="assets/title_icon.png"))
+
+        ## Creating a container
+        container = ctk.CTkFrame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        ## Initialize Frames
+        self.frames = {}
+        self.LoginPage = LoginPage
+        self.SignupPage = SignupPage
+        self.RouterDataPage = RouterDataPage
+        self.HomePage = HomePage
+
+        ## Defining Frames and Packing it
+        for F in {LoginPage, SignupPage, RouterDataPage, HomePage}:
+            frame = F(self, container)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky = "nsew")    
+           
+        self.show_frame(LoginPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        #menubar = frame.create_menubar(self)
+        #self.configure(menu=menubar)
+        frame.tkraise()                         ## This line will put the frame on front
+ 
+#---------------------------------------- LOGINPAGE / CONTAINER ------------------------------------------------------------------------
+        
+class LoginPage(ctk.CTkFrame):
+    def __init__(self, parent, container):
+    
+        super().__init__(container)
+
+        label = ctk.CTkLabel(self, text="Login Page")
+        label.pack(pady=0,padx=0)
+        username_entry = ctk.CTkEntry(self, placeholder_text = "Username")
+        username_entry.pack(pady = 12, padx = 10)
+
+        password_entry = ctk.CTkEntry(self, placeholder_text = "Password", show = "*")
+        password_entry.pack(pady = 12, padx = 10)
+
+        button = ctk.CTkButton(self, text = "Login", command = lambda: login(username_entry, password_entry))
+        button.pack(pady = 12, padx = 10)
+
+        checkbox = ctk.CTkCheckBox(self, text = "Remember me")
+        checkbox.pack(pady = 12, padx = 10)
+
+        signup_button = ctk.CTkButton(self, text = "Sign up", cursor = 'hand2', command = lambda: parent.show_frame(parent.SignupPage))
+        signup_button.pack(pady = 1, padx = 10)
+
+        def login(username, password):
+            username = username_entry.get()
+            password = password_entry.get()
+            user_data = user.query.filter_by(username=username).first()
+            if user_data:
+                stored_password = user_data.password
+                if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
+                    parent.show_frame(parent.RouterDataPage)
+                else:
+                    messagebox.showerror("Login Failed", "Incorrect password.")
+            else:
+                messagebox.showerror("Login Failed", "Username not found.")
+        
+#---------------------------------------- SIGNUP-PAGE / CONTAINER ------------------------------------------------------------------------
+
+class SignupPage(ctk.CTkFrame):
+    def __init__(self, parent, container):
+        super().__init__(container)
+
+        label = ctk.CTkLabel(self, text="Signup Page")
+        label.pack(pady=0,padx=0)
+
+        username_entry = ctk.CTkEntry(self, placeholder_text = "Username")
+        username_entry.pack(pady = 12, padx = 10)
+
+        password_entry = ctk.CTkEntry(self, placeholder_text = "Password", show="*")
+        password_entry.pack(pady = 12, padx = 10)
+
+        button = ctk.CTkButton(self, text = "Sign up", command = lambda: signup(username_entry, password_entry, account_type_var))
+        button.pack(pady = 12, padx = 10)
+
+        account_type_label = ctk.CTkLabel(self, text="Account Type:")
+        account_type_label.pack()
+        account_type_var = ctk.StringVar(self)
+        account_type_var.set("standard")  # Default value
+        account_type_dropdown = ctk.CTkOptionMenu(self, variable = account_type_var, values=["admin", "standard"])
+        account_type_dropdown.pack(pady = 4)
+
+        login_label = ctk.CTkLabel(self, text = "Already have an account?")
+        login_label.pack(pady = 12, padx = 10)
+
+        login_button = ctk.CTkButton(self, text = "Login", cursor = 'hand2', command = lambda: parent.show_frame(parent.LoginPage))
+        login_button.pack(pady = 1, padx = 10)
+
+        def signup(username_entry, password_entry, account_type_var):
+            username = username_entry.get()
+            password = password_entry.get()
+            account_type = account_type_var.get()
+            existing_user = user.query.filter_by(username=username).first()
+            if existing_user:
+                messagebox.showerror("Sign Up Failed", "Username already exists.")
+            else:
+                # Hash the password
+                hashed_password = hash_password(password)
+                # Create a new user
+                new_user = user(username=username, password=hashed_password, account_type=account_type)
+                db.session.add(new_user)
+                db.session.commit()
+                # Clear entry fields
+                username_entry.delete(0, 'end')
+                password_entry.delete(0, 'end')
+                messagebox.showinfo("Sign Up Successful", "Your account has been created successfully!")
+
+        def hash_password(password):
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+            return hashed_password.decode('utf-8')
+
+#---------------------------------------- ROUTER-DATA-PAGE / CONTAINER ------------------------------------------------------------------------
+import json
+class RouterDataPage(ctk.CTkFrame):
+    def __init__(self, parent, container):
+        super().__init__(container)
+
+        info = ctk.CTkLabel(self, text = "Please enter the router data")
+        info.pack(pady = 12, padx = 10)
+
+        user_entry = ctk.CTkEntry(self, placeholder_text = "router admin user")
+        user_entry.pack(pady = 12, padx = 10)
+
+        password_entry = ctk.CTkEntry(self, placeholder_text = "router password", show="*")
+        password_entry.pack(pady = 12, padx = 10)
+
+        button = ctk.CTkButton(self, text = "Save", command = lambda: save_router_data(user_entry, password_entry))
+        button.pack(pady = 12, padx = 10)
+
+        def save_router_data(user, password):
+        # Hash the password
+        #hashed_password = hash_password(password_entry.get())
+            user = user_entry
+            password = password_entry
+            data = {
+            "router_user": user.get(),
+            "router_password": password.get(),
+            "ip_address": "192.168.99.1"
+            }
+        
+            with open("router_data.json", "w") as file:
+                json.dump(data, file)
+
+            parent.show_frame(parent.HomePage)
+
+#---------------------------------------- HOME PAGE FRAME / CONTAINER ------------------------------------------------------------------------
+import paramiko
+
+class HomePage(ctk.CTkFrame):
+    def __init__(self, parent, container):
+        super().__init__(container)
+
+        data = open('router_data.json')
+        router_data = json.load(data)
+
+        command = "cat /tmp/dhcp.leases"
+
+        host = router_data['ip_address']
+        username = router_data['router_user']
+        password = router_data['router_password']
+
+        client = paramiko.client.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(host, username=username, password=password)
+        stdin, stdout, stderr = client.exec_command(command)
+        active_clients = stdout.read().decode()
+        client.close()
+
+        dhcp_leases = []
+        for line in active_clients.split('\n'):
+            if line.strip():
+                timestamp, mac_address, ip_address, hostname, client_id = line.split()
+                dhcp_lease = {
+                    "timestamp": timestamp,
+                    "mac_address": mac_address,
+                    "ip_address": ip_address,
+                    "hostname": hostname,
+                    "client_id": client_id
+                }
+                dhcp_leases.append(dhcp_lease)
+
+        with open("active_clients.json", "w") as file:
+            json.dump(dhcp_leases, file, indent = 4)
+        
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        label = ctk.CTkLabel(self, text="Home Page")
+        label.grid(row = 0, column = 0, sticky = E, pady = 20, padx = 10)
+        for i, lease in enumerate(dhcp_leases):
+            client_info = f"MAC Address: {lease['mac_address']}\nIP Address: {lease['ip_address']}\nHostname: {lease['hostname']}\nClient ID: {lease['client_id']}"
+            label = ctk.CTkLabel(self, text=client_info)
+            label.grid(row=i+1, column=0, sticky = E, pady=30, padx=10)
+            button = ctk.CTkButton(self, text="Manage device")
+            button.grid(row=i+1, column=1, sticky = W, pady=45, padx=10)
+
+    def create_menubar(self, parent):
+        menubar = Menu(parent, bd=3, relief=RAISED)
+
+        ## Filemenu
+        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="New Project", command=lambda: parent.show_frame(parent.Validation))
+        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=parent.quit)  
+
+        ## proccessing menu
+        processing_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Validation", menu=processing_menu)
+        processing_menu.add_command(label="validate")
+        processing_menu.add_separator()
+
+        ## help menu
+        help_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About")
+        help_menu.add_separator()
+
+        return menubar
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        App().mainloop()
+        
---
+++ db_creation.py
@@ -0,0 +1,115 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, relationship, Mapped
+from sqlalchemy import Integer, String, Column, VARCHAR, Enum, FLOAT, JSON, TIMESTAMP
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/balin/Desktop/SQLite_DB/net-management.db'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = 'False'
+db = SQLAlchemy(app)
+
+rigths = db.Table( 'rights',
+    db.Column('iduser', db.Integer, db.ForeignKey('user.iduser'), primary_key=True),
+    db.Column('iddevice', db.Integer, db.ForeignKey('device.iddevice'), primary_key=True)
+ )
+
+class user(db.Model):
+    __tablename__ = 'user'
+
+    iduser = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    username = db.Column(db.VARCHAR(45), nullable = False, unique = True)
+    password = db.Column(db.VARCHAR(64), nullable = False)
+    account_type = db.Column(db.Enum('admin', 'standard'))
+    devices = relationship('device', secondary = rigths, back_populates='users')
+    device_settings = relationship('device_setting', back_populates = 'user_')
+
+
+class device(db.Model):
+    __tablename__ = 'device'
+    
+    iddevice = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    device_name = db.Column(db.VARCHAR(45))
+    MAC_address = db.Column(db.VARCHAR(17), nullable = False, unique = True)
+    device_type = db.Column(db.Enum('Router', 'Extender', 'Mobile', 'Laptop', 'Computer', 'TV', 'Other'), nullable = False)
+    users = relationship('user', secondary = rigths, back_populates='devices')
+    device_settings = relationship('device_setting', back_populates = 'device_')
+    outgoing_connections = db.relationship('connection', foreign_keys='connection.iddevice_source', back_populates='source_device')
+    incoming_connections = db.relationship('connection', foreign_keys='connection.iddevice_destination', back_populates='destination_device')
+
+class connection(db.Model):
+    __tablename__ = 'connection'
+
+    iddevice_source = db.Column(db.Integer, db.ForeignKey('device.iddevice'), primary_key = True)
+    iddevice_destination = db.Column(db.ForeignKey('device.iddevice'), primary_key = True)
+    connection_type = db.Column(db.Enum('LAN', 'WLAN'), nullable = False) #WAN nu e necesar
+    speed = db.Column(db.FLOAT)
+    bandwidth = db.Column(db.Enum('2.4GHz', '5GHz')) #banda de frecventa e mandatory doar daca e conexiune wireless
+    source_device = db.relationship('device', foreign_keys=[iddevice_source], back_populates='outgoing_connections')
+    destination_device = db.relationship('device', foreign_keys=[iddevice_destination], back_populates='incoming_connections')
+
+class device_setting(db.Model):
+    __tablename__ = 'device_setting'
+
+    iddevice_setting = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    iduser = db.Column(db.ForeignKey('user.iduser'))
+    iddevice = db.Column(db.ForeignKey('device.iddevice'))
+    idsetting = db.Column(db.ForeignKey('settings.idsetting'))
+    setting_value = db.Column(db.JSON)
+    setting_time = db.Column(db.DateTime, nullable = False)
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    setting = relationship('settings', back_populates = 'device_settings')
+    user_ = relationship('user', back_populates = 'device_settings')
+    device_ = relationship('device', back_populates = 'device_settings')
+
+class settings(db.Model):
+    __tablename__ = 'settings'
+
+    idsetting = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    setting_name = db.Column(db.VARCHAR(45), nullable = False)
+    description = db.Column(db.VARCHAR(100))
+    device_settings = relationship('device_setting', back_populates= 'setting')
+
+
+john_doe = user(username='john_doe', password='password123', account_type='standard')
+susana = user(username='susana50', password='password123', account_type='admin')
+router = device(device_name='Router principal', MAC_address='00:11:22:33:44:55', device_type='Router')
+laptop = device(device_name='laptop iulia', MAC_address='00:22:41:30:47:50', device_type='Laptop')
+setare = device_setting(setting_time = datetime.now(), start_time = datetime(2024, 2, 13, 21, 00, 00))
+blocare_acces = settings(setting_name = 'Blocare acces' , description = 'Blocarea accesului unui device in retea')
+tv = device(device_name = 'TV sufragerie', MAC_address = '00:12:11:30:AF:50', device_type = 'TV')
+
+with app.app_context():
+    db.create_all()
+    # db.session.add(john_doe)
+    # db.session.add(susana)
+    # db.session.add(router)
+    # db.session.add(laptop)
+    # db.session.add(tv)
+    # db.session.add(setare)
+    # db.session.add(blocare_acces)
+    # susana = db.session.query(user).filter_by(username='susana50').first()
+    # router_s = db.session.query(device).filter_by(iddevice=1).first()
+    # susana.devices.append(router_s)
+    # susana = db.session.query(user).filter_by(username='susana50').first()
+    # laptop_s = db.session.query(device).filter_by(iddevice = 2).first()
+    # susana.devices.append(laptop_s)
+    # john_doe = db.session.query(user).filter_by(username = 'john_doe').first()
+    # tv_j = db.session.query(device).filter_by(iddevice = 3).first()
+    # john_doe.devices.append(tv_j)
+    # setare1 = db.session.query(settings).filter_by(idsetting=1).first()
+    # blocare_acces = db.session.query(device_setting).filter_by(iddevice_setting = 1).first()
+    # setare1.device_settings.append(blocare_acces)
+    # blocare_acces = db.session.query(device_setting).filter_by(iddevice_setting = 1).first()
+    # susana = db.session.query(user).filter_by(iduser = 2).first()
+    # susana.device_settings.append(blocare_acces)
+    # blocare_acces = db.session.query(device_setting).filter_by(iddevice_setting = 1).first()
+    # router_s = db.session.query(device).filter_by(iddevice=1).first()
+    # router_s.device_settings.append(blocare_acces)
+    # router_s = db.session.query(device).filter_by(iddevice = 1).first()
+    # laptop_s = db.session.query(device).filter_by(iddevice = 2).first()
+    # connection1 = connection(source_device = router_s, destination_device = laptop_s, connection_type = 'LAN', speed = 512.65)
+    # db.session.add(connection1)
+
+    db.session.commit()
