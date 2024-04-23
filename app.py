a010e7bcf5f4d98cbcaf0e9e047a3a823617b0af
Balint Iulia Maria <balintiuliamaria@outlook.com>
2024-04-23 21:32:50 +0300

    Created template for Managed devices page

--- app.py
+++ app.py
@@ -45,9 +45,10 @@ class App(ctk.CTk):
         self.SignupPage = SignupPage
         self.RouterDataPage = RouterDataPage
         self.HomePage = HomePage
+        self.ManagedDevices = ManagedDevices
 
         ## Defining Frames and Packing it
-        for F in {LoginPage, SignupPage, RouterDataPage, HomePage}:
+        for F in {LoginPage, SignupPage, RouterDataPage, HomePage, ManagedDevices}:
             frame = F(self, container)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky = "nsew")    
@@ -285,16 +286,36 @@ class HomePage(ctk.CTkFrame):
         ## Filemenu
         filemenu = Menu(menubar, tearoff=0, relief=RAISED)
         menubar.add_cascade(label="Devices", menu=filemenu)
-        filemenu.add_command(label="Managed devices", command=lambda: parent.show_frame(parent.Validation))
-        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
+        filemenu.add_command(label="Managed devices", command=lambda: parent.show_frame(parent.ManagedDevices))
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=parent.quit)  
 
-        ## proccessing menu
-        processing_menu = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label="Validation", menu=processing_menu)
-        processing_menu.add_command(label="validate")
-        processing_menu.add_separator()
+        ## help menu
+        help_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About")
+        help_menu.add_separator()
+
+        return menubar
+
+import paramiko
+
+class ManagedDevices(ctk.CTkFrame):
+    def __init__(self, parent, container):
+        super().__init__(container)
+        label = ctk.CTkLabel(self, text="Managed Devices")
+        label.grid(row = 0, column = 0, sticky = E, pady = 20, padx = 10)
+
+
+    def create_menubar(self, parent):
+        menubar = Menu(parent, bd=3, relief=RAISED)
+
+        ## Filemenu
+        filemenu = Menu(menubar, tearoff=0, relief=RAISED)
+        menubar.add_cascade(label="Devices", menu=filemenu)
+        filemenu.add_command(label="Connected devices", command=lambda: parent.show_frame(parent.HomePage))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=parent.quit)  
 
         ## help menu
         help_menu = Menu(menubar, tearoff=0)
